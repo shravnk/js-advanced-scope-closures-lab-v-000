5e17ead677d470839aaf54ef37d5af13694da16e
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 06:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(range) {
   return function(blockOne, blockTwo) {
     dist = blockOne[0,1] - blockTwo[0,2]
-    if(dist > range) {return "${dist - range} blocks out of range"}
+    if(dist > range) {return "${dist} blocks out of range"}
     else {return "within range by ${range - dist}"}
   }
 }
